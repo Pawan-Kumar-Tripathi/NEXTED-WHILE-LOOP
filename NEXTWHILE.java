20652d7e7ae7f9e7008c686fe9210843d1280c99
PAWAN KUMAR TRIPATHI <78968923+Pawan-Kumar-Tripathi@users.noreply.github.com>
2022-06-27 15:21:54 +0530

    Add files via upload

---
+++ NEXTWHILE.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+class NEXTWHILE
+    {
+        public void main()
+        {
+            int i =0;
+            while(i<3)
+            {
+                int j = 0 ;
+                while(j<5)
+                {
+                    System.out.print(j);
+                    j++;
+                }
+                System.out.println(" ");
+                i++;
+            }
+        }
+    }
